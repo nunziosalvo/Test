25c41d2236c6b3e7adbddff832268659f593b36e
salvo <salvo@NB-SALVO.mil.TXTII.IT>
2017-07-06 17:21:54 +0200

    azzz

--- src/Hello.java
+++ src/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		Sum mySum = new Sum(2,3);
 		StringBuilder s = new StringBuilder();
-		s.append("Hello World!");
+		s.append("Hello World xxx!");
 		s.append(mySum.result());
 		
 		System.out.println(s);
